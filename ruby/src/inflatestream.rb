6aae767c9d214f53c9cae71bfee426ff2f921270
Aaron L <aaron@bettercoder.net>
2011-04-25 16:51:16 -0700

    Fixed up the state management issues in Inflate.
    - Made all state to decompression class-wide.
    - Ensured that state is modified properly through calls.

--- ruby/src/inflatestream.rb
+++ ruby/src/inflatestream.rb
@@ -8,27 +8,39 @@ class InflateStream
 	def initialize(stream)
 		@stream = stream
 
-		#Buffer for storing bytes during decompression
-		@buffer = ''
+		#Bit level state ===============
 		#Offset into the stream
 		@offset = 0
 		#Last byte read.
 		@byte = nil
 		#The bit offset into the last byte read
 		@bit = 0
+
+		#Block level state ==============
+		#Compression
+		@compression = 0x3
 		#Is this the final block?
 		@finalblock = false
-		#The tree being used currently for decoding.
-		@tree = nil
-		#The compression method currently being used.
-		@compression = 0x3
-		#The uncompressed length of the stream (for multiple reads)
+		#Uncompressed length of the block
 		@uncompressed_length = nil
 
-		@in_block = false
-
+		#Block operation level state ==============
+		#Offset into buffer where to copy from
 		@repeatoffset = nil
+		#The remaning bytes to copy from the offset.
 		@repeatlength = nil
+
+		#Buffer for storing BufferSize bytes during decompression (for backrefs)
+		@buffer = ''
+		@bufferoffset = 0
+		#The tree being used currently for decoding.
+		@tree = nil
+		#The buffer to write to
+		@userbuffer = nil
+		#The length remaining to write to the buffer
+		@length = nil
+		#The number of bytes written to the user's buffer.
+		@byteswritten = 0
 	end
 
 	# Reads the blocks of the compressed file
@@ -37,27 +49,39 @@ class InflateStream
 	# @param [Buffer] The buffer to store things in.
 	# @return [String|Fixnum] A string if no buffer was supplied or n chars read
 	def read(length = nil, buffer = nil)
+		@length = length
+		@userbuffer = buffer
+		@byteswritten = 0
+		@bufferoffset = @buffer.length
+
+		#Check if we ended reading in a repeat, if so continue writing.
+		if @repeatoffset != nil
+			write_repeat()
+		end #Otherwise read in more blocks
+
 		while true
-			if !@inblock
+			if @compression == 0x3
 				@finalblock = 0x1 == next_bit()
 				@compression = read_compression()
 			end
 
 			case @compression
 				when HuffmanCoding::Uncompressed
-					ret = read_uncompressed_block(length, buffer)
+					read_uncompressed_block()
 				when HuffmanCoding::FixedHuffman
-					ret = read_huffman_block(nil, length, buffer)
+					read_huffman_block()
 				when HuffmanCoding::DynamicHuffman
-					ret = read_huffman_block(read_tree(), length, buffer)
+					read_huffman_block(read_tree())
 				else
 					throw 'Illegal compression method'
 			end
 
-			break if @finalblock
+			break if @finalblock || @byteswritten == @length
 		end
 
-		return ret
+		return @userbuffer != nil ?
+			@byteswritten :
+			@buffer[@bufferoffset..@buffer.length]
 	end
 
 	private
@@ -80,39 +104,36 @@ class InflateStream
 	end
 
 	# Reads the data for the uncompressed block
-	#
-	# @param [Length] The number of bytes to read.
-	# @param [Buffer] The buffer to store things in.
-	# @return [String|Fixnum] A string if no buffer was supplied or n chars read
-	def read_uncompressed_block(length = nil, buffer = nil)
-		if @uncompressed_length == nil
+	def read_uncompressed_block()
+		if @uncompressed_length == nil || @uncompressed_length == 0
 			lengths = @stream.read(4).unpack('vv')
 			length = lengths[0]
-			if (length & ~length)
+			rubyshortcomings = ((2 ** 16)-1) << 16
+			if (length == ~(lengths[1] | rubyshortcomings))
 				throw 'Uncompressed length check failed.'
 			end
 			@uncompressed_length = length
 		end
 
 		if @uncompressed_length == 0
-			if buffer != nil
-				0
-			else
-				nil
-			end
+			@compression = 0x3
+			return
 		end
 
-		if length == nil || length > @uncompressed_length
-			length = @uncompressed_length
+		buf = ''
+		nBytes = @length > @uncompressed_length ? @uncompressed_length : @length
+		@stream.read(nBytes, buf)
+		@buffer.concat(buf)
+		@uncompressed_length -= buf.length
+		@byteswritten += buf.length
+
+		if @userbuffer != nil
+			@userbuffer.concat(buf)
 		end
 
-		readbytes = @stream.read(length, buffer)
-		@uncompressed_length -= readbytes.length
-
-		if buffer != nil
-			readbytes.length
-		else
-			readbytes
+		#This tells us we're no longer in a block
+		if @uncompressed_length == 0
+			@compression = 0x3
 		end
 	end
 
@@ -125,18 +146,12 @@ class InflateStream
 	# Reads the data encoded by a huffman tree.
 	#
 	# @param [Tree] The huffman tree structure to use to decode.
-	# @param [Length] The number of bytes to read.
-	# @param [Buffer] The buffer to store things in.
-	# @return [String|Fixnum] A string if no buffer was supplied or n chars read
-	def read_huffman_block(tree, length = nil, buffer = nil)
+	def read_huffman_block(tree = nil)
 		if (tree == nil)
 			tree = HuffmanCoding::fixed_tree()
 		end
 		
-		written = 0
-		bufferoffset = @buffer.length
-		while length == nil || written < length
-			
+		while @byteswritten != @length
 			tree.begin_find()
 			nextvalue = nil
 			while (nextvalue = tree.query(next_bit())) == nil
@@ -144,52 +159,49 @@ class InflateStream
 
 			if nextvalue < 256
 				@buffer += nextvalue.chr
-				written += 1
+				@byteswritten += 1
+				if @userbuffer != nil
+					@userbuffer.concat(nextvalue.chr)
+				end
 			elsif nextvalue == 256
-				@in_block = false
+				@compression = 0x3
 				break
 			else
-				ret = write_repeat(length, buffer,
-					read_length(nextvalue), read_distance())
-				if buffer != nil
-					written += ret
-				else
-					written += ret.length
-				end
+				write_repeat(read_length(nextvalue), read_distance())
 			end
 		end
 
-		write = @buffer[bufferoffset..(bufferoffset+written)]
-		if buffer != nil
-			buffer += write
-			written
-		else
-			write
-		end
 	end
 
 	# Writes bytes to the buffer based on a length and offset.
 	#
-	# @param [Length] The length of bytes to copy as a maximum.
-	# @param [Buffer] The optionally supplied buffer.
 	# @param [CopyLength] The length to copy from:
 	# @param [Distance] The offset into the buffer to copy from.
-	# @return [String|Fixnum] A string if no buffer was supplied or n chars read
-	def write_repeat(length = nil, buffer = nil, copylength, distance)
-		@repeatlength = copylength
-		@repeatoffset = distance
+	def write_repeat(copylength = nil, distance = nil)
+		if copylength != nil
+			@repeatlength = copylength
+		end
+		if distance != nil
+			@repeatoffset = distance
+		end
 
 		str = ''
 		@repeatlength.times { |i|
 			byte = @buffer[@buffer.length-@repeatoffset]
 			@buffer += byte
-			str += byte
+			if @userbuffer != nil
+				@userbuffer.concat(byte)
+			end
+			@byteswritten += 1
+			@repeatlength -= 1
+
+			break if @byteswritten == @length
 		}
-		if buffer != nil
-			buffer += str
-			str.length
-		else
-			str
+
+		#Make sure state represents finished.
+		if @repeatlength == 0
+			@repeatlength = nil
+			@repeatoffset = nil
 		end
 	end
 
--- ruby/src/tests/inflatestream_tests.rb
+++ ruby/src/tests/inflatestream_tests.rb
@@ -16,6 +16,34 @@ class TestInflateStream < Test::Unit::TestCase
 		assert_equal('aoeaoeaoeae', @i.read())
 	end
 
+	def test_read_with_lengths
+		assert_equal('a', @i.read(1))
+		assert_equal('oe', @i.read(2))
+		assert_equal('aoe', @i.read(3))
+		assert_equal('aoea', @i.read(4))
+		assert_equal('e', @i.read(5))
+	end
+
+	def test_read_with_buffers
+		buf = ''
+		assert_equal(1, @i.read(1, buf))
+		assert_equal(2, @i.read(2, buf))
+		assert_equal(3, @i.read(3, buf))
+		assert_equal(4, @i.read(4, buf))
+		assert_equal(1, @i.read(5, buf))
+		assert_equal('aoeaoeaoeae', buf)
+	end
+
+	def test_uncompressed
+		mem = MemoryFile.new([
+			0x1, 0xD, 0x0, ~0xD, ~0x0
+		].concat('hello friends'.unpack('c*')))
+		
+		@i = InflateStream.new(mem)
+		assert_equal('hello ', @i.read(6))
+		assert_equal('friends', @i.read(256))
+	end
+
 	def test_next_bit
 		expected = 0xCC4B
 		(0..15).each { |j|
